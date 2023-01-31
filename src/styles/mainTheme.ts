4ab49f14e446f9457d4b48e45278cab759f0e46f
Xavier Sans <xavi.sans.b@gmail.com>
2023-01-31 22:12:27 +0100

    Add mainTheme

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    mainColors: {
+      dark: "#2d2d2d",
+      gray: "#EEEEEE",
+    },
+    error: {
+      base: "#f77272",
+    },
+    success: {
+      base: "#67e5ce",
+    },
+  },
+
+  fontWeights: {
+    bold: 700,
+    regular: 400,
+  },
+};
+
+export default mainTheme;
