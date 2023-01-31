d89c1d08b61cb26e7eae5e466e0e619c5ddcce35
Xavier Sans <xavi.sans.b@gmail.com>
2023-01-31 14:46:09 +0100

    Re-do pokemon context

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import PokemonContextProvider from "./stores/contexts/pokemonContext/PokemonContextProvider";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <PokemonContextProvider>
+      <App />
+    </PokemonContextProvider>
   </React.StrictMode>
 );
 
