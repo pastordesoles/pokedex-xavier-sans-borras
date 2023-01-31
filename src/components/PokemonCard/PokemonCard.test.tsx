c367efe7726d6bc929dd1367298bdbaca5fcd2f2
Xavier Sans <xavi.sans.b@gmail.com>
2023-01-31 20:26:33 +0100

    Add PokemonCard test

---
+++ src/components/PokemonCard/PokemonCard.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import TestWrapper from "../../mocks/wrappers/TestWrapper";
+import PokemonCard from "./PokemonCard";
+import mockPokemonResponse from "../../mocks/mockResponses/mockPokemonResponse";
+
+describe("Given a PokemonCard component", () => {
+  describe("When it's rendered with the Pokemon 'charmander'", () => {
+    test("Then it should show a level 3 heading with the Pokemon name 'Charmander", () => {
+      render(<PokemonCard pokemon={mockPokemonResponse[0]} />, {
+        wrapper: TestWrapper,
+      });
+      const pokemonAccessibleName = "Charmander";
+
+      const pokemonName = screen.getByRole("heading", {
+        name: `${pokemonAccessibleName}`,
+      });
+
+      expect(pokemonName).toHaveTextContent(pokemonAccessibleName);
+    });
+  });
+});
