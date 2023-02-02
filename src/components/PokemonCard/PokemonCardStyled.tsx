0c5af787685a7aa4afad4744586e0d261822b58a
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-02 09:18:06 +0100

    Fix html semanthics in pokemon card

--- src/components/PokemonCard/PokemonCardStyled.tsx
+++ src/components/PokemonCard/PokemonCardStyled.tsx
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 
 const PokemonCardStyled = styled.li`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 2rem;
-  border: 1px solid black;
-  height: 250px;
-  width: 250px;
-  background-color: white;
+  article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+    border: 1px solid black;
+    height: 250px;
+    width: 250px;
+    background-color: white;
+  }
 
   @media only screen and (min-width: 1150px) {
     :hover {
