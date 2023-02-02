79bc3da43023cd43b5ee1d8cdee1a3cbb215d6b3
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-02 11:09:38 +0100

    Add loader test to details page

--- src/pages/Details/Details.test.tsx
+++ src/pages/Details/Details.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import LoaderTrueWrapper from "../../mocks/wrappers/LoaderTrueWrapper";
 import TestWrapper from "../../mocks/wrappers/TestWrapper";
 import Details from "./Details";
 
@@ -10,6 +11,16 @@ describe("Given a Details component", () => {
       const pokemonList = screen.getAllByRole("listitem");
 
       expect(pokemonList).toHaveLength(2);
+    });
+  });
+
+  describe("When it's rendered and isLoading is true", () => {
+    test("Then it should show a spinning loader", () => {
+      render(<Details />, { wrapper: LoaderTrueWrapper });
+
+      const loadingGif = screen.getByTestId("custom-loading");
+
+      expect(loadingGif).toBeInTheDocument();
     });
   });
 });
