4c408ce55ae4a060beb3d9b04c51af5e0159dfc6
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-02 11:36:07 +0100

    Remove lazy load

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,6 @@
-import { lazy, Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Details from "../../pages/Details/Details";
 import HomePage from "../../pages/HomePage/HomePage";
-import Loader from "../Loader/Loader";
-
-const Details = lazy(() => import("../../pages/Details/Details"));
 
 const App = (): JSX.Element => {
   return (
@@ -11,14 +8,7 @@ const App = (): JSX.Element => {
       <Route path="/" element={<Navigate to="/pokemon" />} />
       <Route path="/pokemon" element={<HomePage />} />
       <Route path="*" element={<Navigate to="/pokemon" />} />
-      <Route
-        path="/pokemon/:pokemonName"
-        element={
-          <Suspense fallback={<Loader />}>
-            <Details />
-          </Suspense>
-        }
-      />
+      <Route path="/pokemon/:pokemonName" element={<Details />} />
     </Routes>
   );
 };
