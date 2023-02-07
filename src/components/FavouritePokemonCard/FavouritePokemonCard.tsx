c1cec7a3cec0520e134489a3f16bf42c0b440b76
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-07 13:22:53 +0100

    Create favourite Card component

---
+++ src/components/FavouritePokemonCard/FavouritePokemonCard.tsx
@@ -0,0 +1,63 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import useApi from "../../hooks/useApi";
+import { PokemonStats } from "../../hooks/types";
+
+interface FavouritePokemonCardProps {
+  pokemon: PokemonStats;
+}
+
+const FavouritePokemonCard = ({
+  pokemon: { abilities, height, id, image, name, types },
+}: FavouritePokemonCardProps): JSX.Element => {
+  const { deleteOnePokemon } = useApi();
+
+  return (
+    <>
+      <li>
+        <article className="pokemon-card">
+          <div className="pokemon-card-basic">
+            <img
+              className="pokemon-card-basic__image"
+              src={image}
+              alt={name}
+              width="100"
+              height="100"
+              loading="lazy"
+            ></img>
+
+            <h3 className="pokemon-card-basic__title">{name}</h3>
+          </div>
+
+          <FontAwesomeIcon
+            className="delete"
+            icon={faXmark}
+            onClick={() => deleteOnePokemon(`${id}`)}
+          />
+
+          <div className="pokemon-card-stats">
+            <span>
+              <span className="bold">Type: </span>
+              {types}
+            </span>
+            <span>
+              <span className="bold">Height: </span>
+              {height}
+            </span>
+            <span>
+              <span className="bold">Abilities:</span>
+            </span>
+            {abilities && (
+              <ul>
+                <li> {abilities.abilityOne}</li>
+                <li> {abilities.abilityTwo}</li>
+              </ul>
+            )}
+          </div>
+        </article>
+      </li>
+    </>
+  );
+};
+
+export default FavouritePokemonCard;
