d80e1cb47bf3da285aadd1061d7e83f5565fad7f
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-07 22:53:53 +0100

    Add pending hooks tests

--- src/hooks/useApi.test.ts
+++ src/hooks/useApi.test.ts
@@ -5,9 +5,14 @@ import TestWrapper from "../mocks/wrappers/TestWrapper";
 import {
   isLoadingFalseActionCreator,
   isLoadingTrueActionCreator,
+  openModalActionCreator,
 } from "../stores/actions/uiActions/uiActionCreators";
+import mockPokemonContext from "../mocks/mockContexts/mockPokemonContext";
+import { mockPokemonDetail } from "../mocks/mockResponses/mockPokemonResponse";
+import { loadFavouritePokemonActionsCreator } from "../stores/actions/pokemonActions/pokemonActionCreators";
 
 const { dispatch: dispatchUi } = mockUiContextFalse;
+const { dispatch: dispatchPokemon } = mockPokemonContext;
 
 describe("Given the useApi hook", () => {
   describe("When its method loadAllPokemon is invoked and an error is thrown", () => {
@@ -122,6 +127,130 @@ describe("Given the useApi hook", () => {
       });
 
       await loadPokemonDetail(searchName);
+
+      expect(dispatchUi).toHaveBeenNthCalledWith(1, firstUiDispatchAction);
+      expect(dispatchUi).toHaveBeenNthCalledWith(2, secondUiDispatchAction);
+    });
+  });
+
+  describe("When its method loadAllFavouritePokemon is invoked", () => {
+    test("Then dispatchUi should be called two times to show and hide loading and to load the received Pokemon", async () => {
+      const firstUiDispatchAction = isLoadingTrueActionCreator();
+      const secondUiDispatchAction = isLoadingFalseActionCreator();
+      const thirdUiDispatchAction = openModalActionCreator(
+        true,
+        "Better reload again..."
+      );
+      const {
+        result: {
+          current: { loadAllFavouritePokemon },
+        },
+      } = renderHook(() => useApi(), {
+        wrapper: TestWrapper,
+      });
+
+      await loadAllFavouritePokemon();
+
+      expect(dispatchUi).toHaveBeenNthCalledWith(1, firstUiDispatchAction);
+      expect(dispatchUi).toHaveBeenNthCalledWith(2, secondUiDispatchAction);
+      expect(dispatchUi).toHaveBeenNthCalledWith(3, thirdUiDispatchAction);
+    });
+  });
+
+  describe("When its method loadAllFavouritePokemon is invoked and an error is thrown", () => {
+    test("Then dispatch should be called one time to show a loading", async () => {
+      const firstUiDispatchAction = isLoadingTrueActionCreator();
+      const secondUiDispatchAction = isLoadingFalseActionCreator();
+      const thirdDispatchAction = loadFavouritePokemonActionsCreator([
+        mockPokemonDetail,
+      ]);
+      const {
+        result: {
+          current: { loadAllFavouritePokemon },
+        },
+      } = renderHook(() => useApi(), {
+        wrapper: TestWrapper,
+      });
+
+      await loadAllFavouritePokemon();
+
+      expect(dispatchUi).toHaveBeenNthCalledWith(1, firstUiDispatchAction);
+      expect(dispatchUi).toHaveBeenNthCalledWith(2, secondUiDispatchAction);
+      expect(dispatchPokemon).toHaveBeenNthCalledWith(1, thirdDispatchAction);
+    });
+  });
+
+  describe("When its method deleteOnePokemon is invoked and the server responds with network error", () => {
+    test("Then dispatch ui should be called with an open modal action", async () => {
+      const {
+        result: {
+          current: { deleteOnePokemon },
+        },
+      } = renderHook(() => useApi(), {
+        wrapper: TestWrapper,
+      });
+      const idToDelete = 1;
+
+      await deleteOnePokemon(`${idToDelete}`);
+
+      expect(dispatchUi).toHaveBeenCalled();
+    });
+  });
+
+  describe("When its method deleteOnePokemon is invoked with the id 1", () => {
+    test("Then dispatch UI should be called with isLoadingFalse action", async () => {
+      const firstUiDispatchAction = isLoadingTrueActionCreator();
+      const secondUiDispatchAction = isLoadingFalseActionCreator();
+
+      const {
+        result: {
+          current: { deleteOnePokemon },
+        },
+      } = renderHook(() => useApi(), {
+        wrapper: TestWrapper,
+      });
+      const idToDelete = "2";
+
+      await deleteOnePokemon(idToDelete);
+
+      expect(dispatchUi).toHaveBeenNthCalledWith(1, firstUiDispatchAction);
+      expect(dispatchUi).toHaveBeenNthCalledWith(2, secondUiDispatchAction);
+    });
+  });
+
+  describe("When its method addOnePokemon is invoked", () => {
+    test("Then dispatch UI should be called with isLoadingFalse action", async () => {
+      const firstUiDispatchAction = isLoadingTrueActionCreator();
+      const secondUiDispatchAction = isLoadingFalseActionCreator();
+      const {
+        result: {
+          current: { addOnePokemon },
+        },
+      } = renderHook(() => useApi(), {
+        wrapper: TestWrapper,
+      });
+
+      await addOnePokemon(mockPokemonDetail);
+
+      expect(dispatchUi).toHaveBeenNthCalledWith(1, firstUiDispatchAction);
+      expect(dispatchUi).toHaveBeenNthCalledWith(2, secondUiDispatchAction);
+    });
+  });
+
+  describe("When its method addOnePokemon is invoked and the server responds with network error", () => {
+    test("Then dispatch UI should be called with isLoadingFalse action", async () => {
+      const firstUiDispatchAction = isLoadingTrueActionCreator();
+      const secondUiDispatchAction = isLoadingFalseActionCreator();
+
+      const {
+        result: {
+          current: { addOnePokemon },
+        },
+      } = renderHook(() => useApi(), {
+        wrapper: TestWrapper,
+      });
+
+      await addOnePokemon(mockPokemonDetail);
 
       expect(dispatchUi).toHaveBeenNthCalledWith(1, firstUiDispatchAction);
       expect(dispatchUi).toHaveBeenNthCalledWith(2, secondUiDispatchAction);
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,6 +10,11 @@ let apiUrl = process.env.REACT_APP_API_URL!;
 let pokemonName = process.env.REACT_APP_API_URL_DETAILS_ONE!;
 let pokemonNameTwo = process.env.REACT_APP_API_URL_DETAILS_TWO!;
 let pokemonNameThree = process.env.REACT_APP_API_URL_DETAILS_THREE!;
+let apiUrlLocal = process.env.REACT_APP_API_URL_LOCAL!;
+let apiUrlLocalDeleteOne = process.env.REACT_APP_API_URL_LOCAL_DELETE_ONE!;
+let apiUrlLocalDeleteTwo = process.env.REACT_APP_API_URL_LOCAL_DELETE_TWO!;
+let apiUrlLocalAdd = process.env.REACT_APP_API_URL_LOCAL_ADD_ONE!;
+let apiUrlLocalAddTwo = process.env.REACT_APP_API_URL_LOCAL_ADD_TWO!;
 
 const handlers = [
   rest.get(`${apiUrl}`, (request, response, context) => {
@@ -37,6 +42,33 @@ const handlers = [
       context.status(200),
       context.json(mockPokemonDetailUndefinedRaw)
     );
+  }),
+
+  rest.get(`${apiUrlLocal}list`, (request, response, context) => {
+    return response.once(context.status(404));
+  }),
+
+  rest.get(`${apiUrlLocal}list`, (request, response, context) => {
+    return response(
+      context.status(200),
+      context.json({ pokemon: [mockPokemonDetail] })
+    );
+  }),
+
+  rest.delete(`${apiUrlLocalDeleteOne}`, (request, response, context) => {
+    return response.networkError("Error");
+  }),
+
+  rest.delete(`${apiUrlLocalDeleteTwo}`, (request, response, context) => {
+    return response(context.status(200));
+  }),
+
+  rest.post(`${apiUrlLocalAdd}`, (request, response, context) => {
+    return response.once(context.status(201));
+  }),
+
+  rest.post(`${apiUrlLocalAddTwo}`, (request, response, context) => {
+    return response.networkError("Error");
   }),
 ];
 
