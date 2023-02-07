7a6d1ff181e4728d5ad8e4e54825e0c853379264
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-07 20:55:56 +0100

    Add actions creators test for modal

--- src/stores/actions/uiActions/uiActionCreators.test.ts
+++ src/stores/actions/uiActions/uiActionCreators.test.ts
@@ -1,6 +1,8 @@
 import {
+  closeModalActionCreator,
   isLoadingFalseActionCreator,
   isLoadingTrueActionCreator,
+  openModalActionCreator,
 } from "./uiActionCreators";
 import UiActionTypes from "./uiActionTypes";
 
@@ -27,6 +29,38 @@ describe("Given the function isLoadingFalseActionCreator", () => {
       const expectedValue = UiActionTypes.isLoadingFalse;
 
       const setIsLoadingAction = isLoadingFalseActionCreator();
+
+      expect(setIsLoadingAction).toHaveProperty(
+        expectedProperty,
+        expectedValue
+      );
+    });
+  });
+});
+
+describe("Given the function openModalActionCreator", () => {
+  describe("When it's invoked", () => {
+    test("Then it should return an action with type 'openModal'", () => {
+      const expectedProperty = "type";
+      const expectedValue = UiActionTypes.openModal;
+
+      const setIsLoadingAction = openModalActionCreator(true, "Hello");
+
+      expect(setIsLoadingAction).toHaveProperty(
+        expectedProperty,
+        expectedValue
+      );
+    });
+  });
+});
+
+describe("Given the function closeModalActionCreator", () => {
+  describe("When it's invoked", () => {
+    test("Then it should return an action with type 'closeModal'", () => {
+      const expectedProperty = "type";
+      const expectedValue = UiActionTypes.closeModal;
+
+      const setIsLoadingAction = closeModalActionCreator();
 
       expect(setIsLoadingAction).toHaveProperty(
         expectedProperty,
