280b9481cc5b8f390336b8c372f05aa24df64abb
Xavier Sans <xavi.sans.b@gmail.com>
2023-02-07 21:13:12 +0100

    Add uiReducer modal actions

--- src/stores/reducers/uiReducer/uiReducer.test.ts
+++ src/stores/reducers/uiReducer/uiReducer.test.ts
@@ -1,5 +1,9 @@
 import { mockCurrentUiStateFalse } from "../../../mocks/mockContexts/mockCurrentUiState";
-import { UiAction } from "../../actions/uiActions/actions";
+import {
+  CloseModalAction,
+  OpenModalAction,
+  UiAction,
+} from "../../actions/uiActions/actions";
 import UiActionTypes from "../../actions/uiActions/uiActionTypes";
 import { CurrentUiState } from "../../contexts/uiContext/types";
 import uiReducer from "./uiReducer";
@@ -9,6 +13,8 @@ describe("Given the function uiReducer", () => {
     test("Then it should return a newUiState with the property 'isLoading' true", () => {
       const newUiState: CurrentUiState = {
         isLoading: true,
+        isOpen: false,
+        modalInformation: { isError: false, modalText: "Hello" },
       };
 
       const action: UiAction = {
@@ -30,10 +36,59 @@ describe("Given the function uiReducer", () => {
     test("Then it should return a newUiState with the property 'isLoading' false", () => {
       const newUiState: CurrentUiState = {
         isLoading: false,
+        isOpen: false,
+        modalInformation: { isError: false, modalText: "Hello" },
       };
 
       const action: UiAction = {
         type: UiActionTypes.isLoadingFalse,
+      };
+
+      const currentUiState = mockCurrentUiStateFalse;
+
+      const expectedUiState = uiReducer(
+        currentUiState as CurrentUiState,
+        action
+      );
+
+      expect(expectedUiState).toStrictEqual(newUiState);
+    });
+  });
+
+  describe("When it receives a currentUiState and an action 'openModal'", () => {
+    test("Then it should return a newUiState with the property 'isOpen' true", () => {
+      const newUiState: CurrentUiState = {
+        isLoading: false,
+        isOpen: true,
+        modalInformation: { isError: false, modalText: "Hello" },
+      };
+
+      const action: OpenModalAction = {
+        type: UiActionTypes.openModal,
+        payload: { isError: false, modalText: "Hello" },
+      };
+
+      const currentUiState = mockCurrentUiStateFalse;
+
+      const expectedUiState = uiReducer(
+        currentUiState as CurrentUiState,
+        action
+      );
+
+      expect(expectedUiState).toStrictEqual(newUiState);
+    });
+  });
+
+  describe("When it receives a currentUiState and an action 'closeModal'", () => {
+    test("Then it should return a newUiState with the property 'isOpen' false", () => {
+      const newUiState: CurrentUiState = {
+        isLoading: false,
+        isOpen: false,
+        modalInformation: { isError: false, modalText: "Hello" },
+      };
+
+      const action: CloseModalAction = {
+        type: UiActionTypes.closeModal,
       };
 
       const currentUiState = mockCurrentUiStateFalse;
